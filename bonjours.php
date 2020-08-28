c8ef9889d418be35d973ca0c1fa5922ddc25cafb
Aymeric Herchuelz <aymericherchuelz19@gmail.com>
2020-08-28 17:49:27 +0200

    add base form

--- bonjours.php
+++ bonjours.php
@@ -4,5 +4,19 @@
  </head>
  <body>
  <?php echo '<p>Bonjour le monde</p>'; ?>
- </body>
+ <html>
+    <head><title>Ma page d'accueil</title></head>
+    <body>
+        <h1>Bienvenue sur le site de toto </h1>
+        <h2>Commencez-donc par vous inscrire :</h2>
+        <form name="inscription" method="post" action="saisie.php">
+            Entrez votre pseudo : <input type="text" name="pseudo"/> <br/>
+            Entrez votre ville : <input type="text" name="ville"/><br/>
+            <input type="submit" name="valider" value="OK"/>
+        </form>
+    </body>
+</html>e="submit" value="OK"></p>
+</form>
+</body>
+
 </html>
